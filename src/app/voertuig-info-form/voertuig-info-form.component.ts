a7cc79b822fcce004ec677fe981291fce8211c9e
NicoleAMS <nicole.sentis@gmail.com>
2021-11-25 11:53:37 +0100

    eerste opzet dynamic form

--- src/app/voertuig-info-form/voertuig-info-form.component.ts
+++ src/app/voertuig-info-form/voertuig-info-form.component.ts
@@ -7,9 +7,40 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VoertuigInfoFormComponent implements OnInit {
 
+  // data
+  voertuigen = [
+    {
+      'id': 0,
+      'type': 'auto',
+      'subtypes': ['Hatchback', 'Sedan', 'Station', 'Cabriolet', 'Coupe', 'Multi Purpose Vehicle (MVP)', 'Terreinauto'],
+      'image': '/assets/auto.jpg'
+    },
+    {
+      'id': 1,
+      'type': 'motor',
+      'subtypes': ['All-road', 'Naked', 'Enduro', 'Race', 'Toermotor', 'Chopper', 'Zijspan'],
+      'image': '/assets/motor.jpg'
+    },
+    {
+      'id': 2,
+      'type': 'scooter',
+      'subtypes': [],
+      'image': '/assets/scooter.jpg'
+    }
+  ]
+  gekozenVoertuig = this.voertuigen[0].type;
+  index: number = 0;
+  kenteken: string = '';
+  kentekenGeldig: boolean = true;
+
   constructor() { }
 
   ngOnInit(): void {
+  }
+
+  selectVoertuig(event: any) {
+    this.index = event.target.options['selectedIndex'];
+    this.gekozenVoertuig = event.target.value;
   }
 
 }
